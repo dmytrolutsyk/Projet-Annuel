240a8e7641be154cad7e37fea8f433ead84945d6
dmytrolutsyk <49659495+dmytrolutsyk@users.noreply.github.com>
2020-05-06 22:22:35 +0200

    exemple upload image none functionnal

--- api-client/server.js
+++ api-client/server.js
@@ -12,6 +12,12 @@ const app = express();
 const url = process.env.MONGODB_URI || 'mongodb://localhost:' + 27017 + '/annonces';
 const dbName = "annonces";
 const JWT_SIGN_SECRET ='secret';
+//IMPORTS DE TEST POUR IMAGE
+// let fs = require('fs');
+// let Gridfs = require('gridfs-stream');
+// let multiparty = require('multiparty');
+// var im = require('imagemagick');
+// let mime = require('mime-types');
 
 function generateTokenForUser(userData) {
     return jwt.sign({
@@ -461,5 +467,62 @@ app.delete('/users/:id', async function(req, res) {
     });
 });
 
+// app.put('/image', async function(req, res) {
+//   var token = req.get('x-access-token');
+//   jwt.verify(token, JWT_SIGN_SECRET, async (err, data) => {
+//         if (err) {
+//             res.status(401).send('Utilisateur non connecté');
+//         } else {   
+//             let gfs = new Gridfs(connected, db); // on instancie l'objet GridFS
+//             let form = new multiparty.Form({maxFilesSize: 1024 * 1024 * 2});
+//             form.parse(req, function (err, fields, files) { //On parse les informations du formulaire
 
+//                 if( mime.lookup(files.file[0].path) === 'image/png' ||
+//                 mime.lookup(files.file[0].path) === 'image/jpeg' ||
+//                 mime.lookup(files.file[0].path) === 'image/gif' ||
+//                 mime.lookup(files.file[0].path) === 'image/pjpeg' ||
+//                 mime.lookup(files.file[0].path) ==='image/x-png')
+//                 {
+//                 im.identify(files.file[0].path, function(err, features){
 
+//                     if(err) console.log(err);
+
+//                     if (!err) {
+
+//                         let writestream = gfs.createWriteStream({
+//                         filename: files.file[0].originalFilename,
+//                         mode: 'w',
+//                         content_type: mime.lookup(files.file[0].path),
+//                         metadata: {
+//                             width : features.width,
+//                             height: features.height,
+//                             extension: files.file[0].originalFilename.split('.').pop()
+//                         }
+//                         });
+
+//                         fs.createReadStream(files.file[0].path).pipe(writestream);
+
+//                         writestream.on('close', function (file) {
+
+//                         fs.unlink(files.file[0].path, function (err) {
+//                             if (err) console.log(err);
+//                             // handle error
+//                             //console.log('success!');
+
+//                             return res.send(file);
+//                         });
+
+//                         });
+//                     }
+//                     });
+
+//                 }
+//                 else {
+//                 return res.status(400).send({err : 'err type file, only png, gif or jpeg, type : '
+//                 + mime.lookup(files.file[0].path)});
+//                 }
+
+//             });
+//         }
+//     });
+// });
