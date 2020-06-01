5e58e0eb434fc228e1fe47cecb38944371a3550e
dmytrolutsyk <49659495+dmytrolutsyk@users.noreply.github.com>
2020-06-01 18:17:43 +0200

    try to request api

--- f&t_Android/app/src/main/java/com/example/ft_android/MainActivity.kt
+++ f&t_Android/app/src/main/java/com/example/ft_android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ft_android
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -27,38 +28,6 @@ class MainActivity : AppCompatActivity() {
             var Password = findViewById<View>(R.id.PasswordField) as EditText
             val Passwordcpt = Password.text.toString()
 
-            ////////////////////////////////////////////////////////////////////////////////////////
-            /*
-            //Variable declaration
-            var mAPIService: APIService? = null
-            //After oncreate
-            mAPIService = ApiUtils.apiService
-            //Some Button click
-
-            mAPIService!!.registrationPost("totomaster", "123456").enqueue(object :
-                Callback<Registration> {
-                override fun onResponse(
-                    call: Call<Registration>,
-                    response: Response<Registration>
-                ) {
-
-                    Log.i("", "post submitted to API." + response.body()!!)
-
-                    if (response.isSuccessful()) {
-
-                        Log.i("", "post registration to API" + response.body()!!.toString())
-                        Log.i("", "post status to API" + response.body()!!.status)
-                        Log.i("", "post msg to API" + response.body()!!.messages)
-
-                    }
-                }
-
-                override fun onFailure(call: Call<Registration>, t: Throwable) {
-
-                }
-            })*/
-            ////////////////////////////////////////////////////////////////////////////////////////
-
             val intent = Intent(this, Homepage::class.java)
             intent.putExtra("Username", Usernamecpt)
             intent.putExtra("Password", Passwordcpt)
@@ -79,6 +48,8 @@ class MainActivity : AppCompatActivity() {
 
                         override fun onResponse(call: Call<DefaultResponse>, response: Response<DefaultResponse>) {
                             Toast.makeText(applicationContext, response.body()?.message, Toast.LENGTH_LONG).show()
+                            Log.i("", "post status to API" + response.body()!!.message)
+                            Toast.makeText(applicationContext,"yallah", duration).show()
                         }
 
                     })
--- f&t_Android/app/src/main/java/com/example/ft_android/Retrofit/RetrofitClient.kt
+++ f&t_Android/app/src/main/java/com/example/ft_android/Retrofit/RetrofitClient.kt
@@ -1,14 +1,13 @@
 package com.example.ft_android.Retrofit
 
-import android.util.Base64
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private val AUTH =
-        "Basic " + Base64.encodeToString("belalkhan:123456".toByteArray(), Base64.NO_WRAP)
+    //private val AUTH =
+    //    "Basic " + Base64.encodeToString("belalkhan:123456".toByteArray(), Base64.NO_WRAP)
 
     private const val BASE_URL = "https://findandtrade.herokuapp.com/"
 
