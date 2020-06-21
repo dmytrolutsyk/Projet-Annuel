0b557128cb3b03f2a454890d28540725a75a4004
dmytrolutsyk <49659495+dmytrolutsyk@users.noreply.github.com>
2020-06-21 17:31:48 +0200

    bottomactivitybar worked

---
+++ f&t_Android/app/src/main/java/com/example/ft_android/ui/account/AccountViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ft_android.ui.account
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AccountViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ f&t_Android/app/src/main/java/com/example/ft_android/ui/home/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.ft_android.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.ft_android.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        var DeconnexionBtn = findViewById<View>(R.id.DeconnexionBtn)
+
+        //FINIR L'IMPLEMENTATION DU BOUTTON DECONNEXION PUTAIN
+        //val textView: TextView = root.findViewById(R.id.text_home)
+        //homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        //    textView.text = it
+        //})
+        return root
+    }
+}
